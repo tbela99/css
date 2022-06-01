809d922a42e8787b912d5cf419458b3e58da94d6
Thierry Bela <bnanga@gmail.com>
2022-06-01 18:12:06 -0400

    incorrectly throwing an exception when parsing @charset

--- test/test.php
+++ test/test.php
@@ -1,65 +1,8 @@
 #!/usr/bin/php
 <?php
 
-use TBela\CSS\Element;
-
 require 'autoload.php';
-$css = '
 
+$parser =  (new \TBela\CSS\Parser("@charset \"utf-8\"; @font-face{font-family:'CenturyGothic';src:url('/CenturyGothic.woff') format('woff');font-weight:400;}", ['capture_errors' => false])) /* ->load('template.min.css') */;
 
-@font-face {
- font-family: "Font Awesome 5 Brands";
- font-style: normal;
- font-weight: 400;
- font-display: block;
- src: url(components/com_sppagebuilder/assets/webfonts/fa-brands-400.woff) format("woff"), url(components/com_sppagebuilder/assets/webfonts/fa-brands-400.ttf) format("truetype")
-}
-.fab {
- font-family: "Font Awesome 5 Brands"
-}
-@font-face {
- font-family: "Font Awesome 5 Free";
- font-style: normal;
- font-weight: 400;
- font-display: block;
- src: url(components/com_sppagebuilder/assets/webfonts/fa-regular-400.woff) format("woff"), url(components/com_sppagebuilder/assets/webfonts/fa-regular-400.ttf) format("truetype")
-}
-.fab,
-.far {
- font-weight: 400
-}
-@font-face {
- font-family: "Font Awesome 5 Free";
- font-style: normal;
- font-weight: 900;
- font-display: block;
- src: url(components/com_sppagebuilder/assets/webfonts/fa-solid-900.woff) format("woff"), url(components/com_sppagebuilder/assets/webfonts/fa-solid-900.ttf) format("truetype")
-}
-';
-
-
-$context = '// @font-face / src / ..';
-
-$element = Element::from($css);
-
-foreach ($element->query('[@name=background][@value*="url("]|[@name=background-image][@value*="url("]|[@name=src][@value*="url("]') as $p) {
-
-    $values = [];
-
-    foreach($p->getRawValue() as $i => $img) {
-
-        if ($img->type == 'css-url' && isset($img->arguments[0]->value)) {
-
-            echo $img->type.' => '.$img->arguments[0]->value."\n";
-
-//            var_dump($img->arguments[0]->value);
-//            $img->arguments[0]->value = '/img/bg'.$i.'.png';
-        }
-
-//        $values[] = $img;
-    }
-
-//    $p->setValue($values);
-}
-
-//echo "$element\n";
+echo (new \TBela\CSS\Renderer(['charset' => true]))->renderAst($parser);
